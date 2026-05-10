ed5d0409fb84d86a79d9e27465903dfc03e7f86d
datrell23 <datrell28@icloud.com>
2026-05-09 19:46:49 -0500

    added Json storge with multiple entries

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import json
+import os
 
 url = "https://www.newegg.com/gigabyte-gv-n5090gaming-oc-32gd-geforce-rtx-5090-32gb-graphics-card-triple-fans/p/N82E16814932761"
 
@@ -22,10 +24,13 @@ title_element = soup.find('h1', class_='product-title')
 def check_title(title_element):
     if title_element:
         print(f"\nFound title element")
-        title_element = title_element.get_text(strip=True)
-        return(f"\nProduct {title_element}")
+        title_text = title_element.get_text(strip=True)
+        return title_text
     else:
-        return("Could not find title")
+        print("Could not find title")
+        return None
+    
+    
 def check_price(price_element):
     if price_element:
         print(f"\nFound price element!")
@@ -36,9 +41,35 @@ def check_price(price_element):
    
         price = float(clean_price)
     
-        return(f"\nCurrent price: ${price}")
+        print(f"\nCurrent price: ${price}")
+        return price
     else:
-        return("Could not find price element.")
+        print("Could not find price element.")
+        return None
 current_time = datetime.now()
 print(check_title(title_element), check_price(price_element))
 print(f"Checked at {current_time}")
+title_text = check_title(title_element)
+price = check_price(price_element)
+
+price_data ={
+    "product": title_text,
+    "price": price,
+    "timestamp": str(current_time),
+    "url": url
+}
+
+file_name = "price_history.json"
+if os.path.exists(file_name):
+    with open(file_name, "r") as f:
+        all_prices = json.load(f)
+else:
+    all_prices = []
+    
+all_prices.append(price_data)
+
+with open(file_name, 'w') as f:
+    json.dump(all_prices, f, indent=4)
+    
+print(f"\n Price Saved to {file_name}")
+print(f"Total entries: {len(all_prices)}")
